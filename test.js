b3bb1356f69c811f63d816869ae0716ef981a5b1
duxinyues <weiyy26445@yunrong.cn>
2023-09-18 13:36:00 +0800

    fix:touch事件

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: duxinyues weiyy26445@yunrong.cn
+ * @Date: 2023-09-17 22:27:14
+ * @LastEditors: duxinyues weiyy26445@yunrong.cn
+ * @LastEditTime: 2023-09-18 12:40:17
+ * @FilePath: /vue-h5/test.js
+ * @Description: 
+ * Copyright (c) 2023 by ${duxinyues} email: ${weiyy26445@yunrong.cn}, All Rights Reserved.
+ */
